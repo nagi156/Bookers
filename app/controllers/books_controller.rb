6a1354a7604e72b3a5c9895b5526e9b6597aae1f
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-03-09 13:50:52 +0000

    [Add] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,10 +33,18 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    
+    if
+      @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+      
+    else
+      flash[:alert] = "Book was not successfully updated."
+      render :edit
+      
+    end
 
   end
 
